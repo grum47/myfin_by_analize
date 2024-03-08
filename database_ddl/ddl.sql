29afc65fb7f63abb3f2a804c3c39fc0fec2f960f
Pavel Evtuhovich <p.evtuhovich.work@gmail.com>
2024-03-08 12:27:07 +0300

    chore(db): corrected ddl

--- database_ddl/ddl.sql
+++ database_ddl/ddl.sql
@@ -2,13 +2,11 @@
 
 Create database myfin;
 
-CREATE SCHEMA myfin;
+CREATE SCHEMA myfin_raw;
+CREATE SCHEMA myfin_dm;
 
-
-CREATE TABLE myfin.myfin_raw (
-	department_id int4 NULL,
+CREATE TABLE myfin_raw.myfin_by (
 	myfin_bank_id int4 NULL,
-	myfin_currencies_courses_bank_id int4 NULL,
 	price_value_usd_sell numeric NULL,
 	price_value_usd_buy numeric NULL,
 	price_value_eur_sell numeric NULL,
@@ -16,14 +14,17 @@ CREATE TABLE myfin.myfin_raw (
 	price_value_rub_sell numeric NULL,
 	price_value_rub_buy numeric NULL,
 	date_page date NULL,
-	price_value_usd_sell_tm timestamp NULL,
-	price_value_usd_buy_tm timestamp NULL,
-	price_value_eur_sell_tm timestamp NULL,
-	price_value_eur_buy_tm timestamp NULL,
-	price_value_rub_sell_tm timestamp NULL,
-	price_value_rub_buy_tm timestamp NULL,
 	bank_name varchar(50) NULL,
-	department_full_address varchar NULL
 );
-CREATE INDEX myfin_raw_bank_name_idx ON myfin.myfin_raw USING btree (bank_name);
-CREATE INDEX myfin_raw_date_page_idx ON myfin.myfin_raw USING btree (date_page);
+CREATE INDEX myfin_raw_bank_name_idx ON myfin_raw.myfin_by USING btree (bank_name);
+CREATE INDEX myfin_raw_date_page_idx ON myfin_raw.myfin_by USING btree (date_page);
+
+CREATE TABLE myfin_raw.cbr_ru (
+	bank_id int4 NOT NULL,
+	price_value_usd_sell numeric NULL,
+	price_value_usd_buy numeric NULL,
+	date_page date NOT NULL,
+	bank_name varchar(50) NOT NULL
+);
+CREATE INDEX cbr_ru_bank_name_idx ON myfin_raw.cbr_ru USING btree (bank_name);
+CREATE INDEX cbr_ru_date_page_idx ON myfin_raw.cbr_ru USING btree (date_page);
